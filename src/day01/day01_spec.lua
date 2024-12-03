c6fd91abb94ee13e2779a32a230bda9a22a8c328
Michael Adler <therisen06@gmail.com>
2024-12-03 20:32:11 +0100

    style: format code

--- src/day01/day01_spec.lua
+++ src/day01/day01_spec.lua
@@ -1,16 +1,16 @@
 local day01 = require("day01")
 
 describe("day01", function()
-	it("solves the example", function()
-		local input = [[3   4
+    it("solves the example", function()
+        local input = [[3   4
 4   3
 2   5
 1   3
 3   9
 3   3
 ]]
-		local part1, part2 = day01.solve(input)
-		assert.are.equal(11, part1)
-		assert.are.equal(31, part2)
-	end)
+        local part1, part2 = day01.solve(input)
+        assert.are.equal(11, part1)
+        assert.are.equal(31, part2)
+    end)
 end)
