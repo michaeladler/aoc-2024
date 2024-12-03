5f3d476e89a6037116657913d790205255a1f5b0
Michael Adler <therisen06@gmail.com>
2024-12-03 20:30:48 +0100

    Day 3: Part 2

--- src/day03/day03.lua
+++ src/day03/day03.lua
@@ -2,14 +2,40 @@
 local M = {}
 
 M.solve = function(input)
-	local part1, part2 = 0, 0
-	for line in input:gmatch("[^\r\n]+") do
-		for x, y in line:gmatch("mul%((%d%d?%d?),(%d%d?%d?)%)") do
-			x, y = tonumber(x), tonumber(y)
-			part1 = part1 + x * y
-		end
-	end
-	return part1, part2
+    local part1, part2 = 0, 0
+    local mul_enabled = true
+    for line in input:gmatch("[^\r\n]+") do
+        local n = string.len(line)
+        local i = 1
+        while i <= n - 3 do
+            local sub = string.sub(line, i, i + 11)
+            -- check for mul
+            local x, y = sub:match("^mul%((%d%d?%d?),(%d%d?%d?)%)")
+            if x and y then
+                local prod = tonumber(x) * tonumber(y)
+                part1 = part1 + prod
+                if mul_enabled then
+                    part2 = part2 + prod
+                end
+                goto continue
+            end
+            -- check for do
+            if sub:match("^do%(%)") then
+                mul_enabled = true
+                goto continue
+            end
+            -- check for don't()
+            if sub:match("^don't%(%)") then
+                mul_enabled = false
+                goto continue
+            end
+
+            ::continue::
+            i = i + 1
+        end
+    end
+
+    return part1, part2
 end
 
 return M
--- src/day03/day03_spec.lua
+++ src/day03/day03_spec.lua
@@ -1,11 +1,16 @@
 local day03 = require("day03")
 
 describe("day03", function()
-	it("solves the example", function()
-		local input = [[xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+    it("solves example 1", function()
+        local input = [[xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 ]]
-		local part1, part2 = day03.solve(input)
-		assert.are.equal(161, part1)
-		-- assert.are.equal(4, part2)
-	end)
+        local part1, _ = day03.solve(input)
+        assert.are.equal(161, part1)
+    end)
+    it("solves example 2", function()
+        local input = [[xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+]]
+        local _, part2 = day03.solve(input)
+        assert.are.equal(48, part2)
+    end)
 end)
