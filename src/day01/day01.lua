358c6d2469ae44d0285a88488d6573004e757989
Michael Adler <therisen06@gmail.com>
2024-12-03 20:32:11 +0100

    style: format code

--- src/day01/day01.lua
+++ src/day01/day01.lua
@@ -2,26 +2,26 @@
 local M = {}
 
 M.solve = function(input)
-	local t1, t2 = {}, {}
-	local count = {}
-	for line in input:gmatch("[^\r\n]+") do
-		local num1, num2 = line:match("(%d+)%s+(%d+)")
-		local x, y = tonumber(num1), tonumber(num2)
-		table.insert(t1, x)
-		table.insert(t2, y)
-		count[y] = (count[y] or 0) + 1
-	end
+    local t1, t2 = {}, {}
+    local count = {}
+    for line in input:gmatch("[^\r\n]+") do
+        local num1, num2 = line:match("(%d+)%s+(%d+)")
+        local x, y = tonumber(num1), tonumber(num2)
+        table.insert(t1, x)
+        table.insert(t2, y)
+        count[y] = (count[y] or 0) + 1
+    end
 
-	table.sort(t1)
-	table.sort(t2)
+    table.sort(t1)
+    table.sort(t2)
 
-	local part1, part2 = 0, 0
-	for i = 1, #t1 do
-		local x, y = t1[i], t2[i]
-		part1 = part1 + math.abs(x - y)
-		part2 = part2 + x * (count[x] or 0)
-	end
-	return part1, part2
+    local part1, part2 = 0, 0
+    for i = 1, #t1 do
+        local x, y = t1[i], t2[i]
+        part1 = part1 + math.abs(x - y)
+        part2 = part2 + x * (count[x] or 0)
+    end
+    return part1, part2
 end
 
 return M
