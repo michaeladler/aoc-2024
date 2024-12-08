c08d181545f3d92fa5ef020c3ec289176d32b4f0
Michael Adler <therisen06@gmail.com>
2024-12-08 15:57:48 +0100

    ci: fix

--- src/day08/day08.lua
+++ src/day08/day08.lua
@@ -1,7 +1,38 @@
 -- Author: Michael Adler
 local M = {}
 
-local Point2D = require("point2d")
+---@class Point2D
+---@field x number
+---@field y number
+local Point2D = {}
+Point2D.__index = Point2D
+
+--- @return Point2D
+function Point2D.new(_, x, y)
+    local instance = setmetatable({}, Point2D)
+    instance.x = x or 0
+    instance.y = y or 0
+    return instance
+end
+
+--- @return Point2D
+function Point2D.__add(a, b)
+    return Point2D:new(a.x + b.x, a.y + b.y)
+end
+
+--- @return Point2D
+function Point2D.__sub(a, b)
+    return Point2D:new(a.x - b.x, a.y - b.y)
+end
+
+--- @return string
+function Point2D:hash()
+    return string.format("%d,%d", self.x, self.y)
+end
+
+function Point2D:__tostring()
+    return string.format("Point2D(%d, %d)", self.x, self.y)
+end
 
 --- @param input string
 M.solve = function(input)
