95c4c4b8a57eb1e07d425f936a75b0f699f9216d
Michael Adler <therisen06@gmail.com>
2024-12-12 20:12:50 +0100

    Day 12, Part 2

--- src/day12/day12.lua
+++ src/day12/day12.lua
@@ -3,21 +3,10 @@ local M = {}
 
 local Point2D = require("aoc_helper.point").Point2D
 
-local all_directions = { Point2D:new(1, 0), Point2D:new(-1, 0), Point2D:new(0, 1), Point2D:new(0, -1) }
+local north, south, east, west = Point2D:new(0, -1), Point2D:new(0, 1), Point2D:new(1, 0), Point2D:new(-1, 0)
+local all_directions = { north, east, south, west } -- order matters, i and i+1 are orthogonal
 
---- @param input string
-M.solve = function(input)
-    local part1, part2 = 0, 0
-
-    local map = {}
-    for line in input:gmatch("[^\r\n]+") do
-        local row = {}
-        for i = 1, string.len(line) do
-            table.insert(row, string.sub(line, i, i))
-        end
-        table.insert(map, row)
-    end
-
+local function solve_part1(map)
     --- @type table<Point2D, boolean>
     local seen = {}
 
@@ -30,7 +19,7 @@ M.solve = function(input)
         end
         seen[start_hash] = true
 
-        local plant = map[start.y][start.x]
+        local plant_start = map[start.y][start.x]
 
         --- @type Point2D[]
         local queue = { start }
@@ -46,7 +35,7 @@ M.solve = function(input)
                     goto continue
                 end
                 local neighbor_plant = row[candidate.x]
-                if neighbor_plant ~= plant then
+                if neighbor_plant ~= plant_start then
                     goto continue
                 end
 
@@ -67,13 +56,108 @@ M.solve = function(input)
     end
 
     local row_count, col_count = #map, #map[1]
+    local total = 0
     for y = 1, row_count do
         for x = 1, col_count do
-            part1 = part1 + calc_price(Point2D:new(x, y))
+            total = total + calc_price(Point2D:new(x, y))
         end
     end
 
-    return part1, part2
+    return total
+end
+
+local function solve_part2(map)
+    -- key insight: number of sides = number of corners
+
+    --- @type table<Point2D, boolean>
+    local seen = {}
+
+    ---@param start Point2D
+    local calc_price = function(start) -- flood fill algorithm
+        -- do not start from a region we've visited before
+        local start_hash = start:hash()
+        if seen[start_hash] then
+            return 0
+        end
+        seen[start_hash] = true
+
+        local plant_start = map[start.y][start.x]
+
+        --- @param p Point2D
+        local count_corners = function(p)
+            local count = 0
+            for i = 1, 4 do
+                local dir1, dir2 = all_directions[i], all_directions[i % 4 + 1]
+                local nb1 = p + dir1 -- e.g. north
+                local nb2 = p + dir2 -- e.g. east
+                local diag = p + dir1 + dir2 -- e.g. north-east (diagonal)
+                local plant_nb1 = (map[nb1.y] or {})[nb1.x]
+                local plant_nb2 = (map[nb2.y] or {})[nb2.x]
+                local plant_diag = (map[diag.y] or {})[diag.x]
+                local ext_corner = plant_nb1 ~= plant_start and plant_nb2 ~= plant_start
+                local int_corner = plant_nb1 == plant_start and plant_nb2 == plant_start and plant_diag ~= plant_start
+                if ext_corner or int_corner then
+                    count = count + 1
+                end
+            end
+            return count
+        end
+
+        --- @type Point2D[]
+        local queue = { start }
+        local area, corners = 1, count_corners(start)
+        repeat
+            local current = table.remove(queue, 1) -- pop
+            for _, dir in ipairs(all_directions) do
+                ---@type Point2D
+                local candidate = current + dir
+
+                local row = map[candidate.y]
+                if not row then
+                    goto continue
+                end
+                local neighbor_plant = row[candidate.x]
+                if neighbor_plant ~= plant_start then
+                    goto continue
+                end
+
+                local candidate_hash = candidate:hash()
+                if not seen[candidate_hash] then
+                    seen[candidate_hash] = true
+                    table.insert(queue, candidate)
+                    area = area + 1
+                    corners = corners + count_corners(candidate)
+                end
+
+                ::continue::
+            end
+        until queue[1] == nil
+
+        return area * corners
+    end
+
+    local row_count, col_count = #map, #map[1]
+    local total = 0
+    for y = 1, row_count do
+        for x = 1, col_count do
+            total = total + calc_price(Point2D:new(x, y))
+        end
+    end
+
+    return total
+end
+
+--- @param input string
+M.solve = function(input)
+    local map = {}
+    for line in input:gmatch("[^\r\n]+") do
+        local row = {}
+        for i = 1, string.len(line) do
+            table.insert(row, string.sub(line, i, i))
+        end
+        table.insert(map, row)
+    end
+    return solve_part1(map), solve_part2(map)
 end
 
 return M
--- src/day12/day12_spec.lua
+++ src/day12/day12_spec.lua
@@ -1,18 +1,22 @@
 local day12 = require("day12")
 
 describe("day12", function()
-    it("solves example 1", function()
+    describe("example 1", function()
         local input = [[AAAA
 BBCD
 BBCC
 EEEC
 ]]
         local part1, part2 = day12.solve(input)
-        assert.are.equal(140, part1)
-        assert.are.equal(0, part2)
+        it("part1", function()
+            assert.are.equal(140, part1)
+        end)
+        it("part2", function()
+            assert.are.equal(80, part2)
+        end)
     end)
 
-    it("solves example 2", function()
+    describe("example 2", function()
         local input = [[OOOOO
 OXOXO
 OOOOO
@@ -20,11 +24,15 @@ OXOXO
 OOOOO
 ]]
         local part1, part2 = day12.solve(input)
-        assert.are.equal(772, part1)
-        assert.are.equal(0, part2)
+        it("part1", function()
+            assert.are.equal(772, part1)
+        end)
+        it("part2", function()
+            assert.are.equal(436, part2)
+        end)
     end)
 
-    it("solves example 3", function()
+    describe("example 3", function()
         local input = [[RRRRIICCFF
 RRRRIICCCF
 VVRRRCCFFF
@@ -37,7 +45,38 @@ MIIISIJEEE
 MMMISSJEEE
 ]]
         local part1, part2 = day12.solve(input)
-        assert.are.equal(1930, part1)
-        assert.are.equal(0, part2)
+        it("part1", function()
+            assert.are.equal(1930, part1)
+        end)
+        it("part2", function()
+            assert.are.equal(1206, part2)
+        end)
+    end)
+
+    describe("example 4", function()
+        local input = [[EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE
+]]
+        local _, part2 = day12.solve(input)
+        it("part2", function()
+            assert.are.equal(236, part2)
+        end)
+    end)
+
+    describe("example 5", function()
+        local input = [[AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA
+]]
+        local _, part2 = day12.solve(input)
+        it("part2", function()
+            assert.are.equal(368, part2)
+        end)
     end)
 end)
