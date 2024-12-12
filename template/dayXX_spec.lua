d234da61197114c95f7c70ab4bc76a7ba5e9cccf
Michael Adler <therisen06@gmail.com>
2024-12-12 18:20:22 +0100

    template: split part1 and part2

--- template/dayXX_spec.lua
+++ template/dayXX_spec.lua
@@ -1,11 +1,15 @@
 local dayXX = require("dayXX")
 
 describe("dayXX", function()
-    it("solves the example", function()
+    describe("example 1", function()
         local input = [[
 ]]
         local part1, part2 = dayXX.solve(input)
-        assert.are.equal(0, part1)
-        assert.are.equal(0, part2)
+        it("part1", function()
+            assert.are.equal(0, part1)
+        end)
+        it("part2", function()
+            assert.are.equal(0, part2)
+        end)
     end)
 end)
