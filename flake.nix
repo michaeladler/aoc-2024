89e44edeb512ca613b2a05251026c3e4e5e65939
Michael Adler <therisen06@gmail.com>
2025-12-28 11:19:03 +0100

    refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -35,63 +35,39 @@
           };
           hp = pkgs.haskell.packages.ghc912;
           hpWithProfiling = hp.extend (extension);
+
+          shellFor =
+            haskellPackages:
+            haskellPackages.shellFor {
+              packages = hpkgs: [
+                # reuse the nixpkgs for this package
+                hpkgs.distribution-nixpkgs
+                # call our generated Nix expression manually
+                (hpkgs.callPackage ./nix/pkg.nix { })
+              ];
+
+              # development tools we use
+              nativeBuildInputs = [
+                pkgs.cabal-install
+                pkgs.cabal2nix
+                pkgs.hpack
+                pkgs.haskell.packages.ghc912.haskell-language-server
+                pkgs.hyperfine
+                pkgs.ormolu
+              ];
+
+              # Extra arguments are added to mkDerivation's arguments as-is.
+              # Since it adds all passed arguments to the shell environment,
+              # we can use this to set the environment variable the `Paths_`
+              # module of distribution-nixpkgs uses to search for bundled
+              # files.
+              # See also: https://cabal.readthedocs.io/en/latest/cabal-package.html#accessing-data-files-from-package-code
+              distribution_nixpkgs_datadir = toString ./distribution-nixpkgs;
+            };
         in
         {
-
-          default = hp.shellFor {
-            packages = hpkgs: [
-              # reuse the nixpkgs for this package
-              hpkgs.distribution-nixpkgs
-              # call our generated Nix expression manually
-              (hpkgs.callPackage ./nix/pkg.nix { })
-            ];
-
-            # development tools we use
-            nativeBuildInputs = [
-              pkgs.cabal-install
-              pkgs.cabal2nix
-              pkgs.hpack
-              pkgs.haskell.packages.ghc912.haskell-language-server
-              pkgs.hyperfine
-              pkgs.ormolu
-            ];
-
-            # Extra arguments are added to mkDerivation's arguments as-is.
-            # Since it adds all passed arguments to the shell environment,
-            # we can use this to set the environment variable the `Paths_`
-            # module of distribution-nixpkgs uses to search for bundled
-            # files.
-            # See also: https://cabal.readthedocs.io/en/latest/cabal-package.html#accessing-data-files-from-package-code
-            distribution_nixpkgs_datadir = toString ./distribution-nixpkgs;
-          };
-
-          profiling = hpWithProfiling.shellFor {
-            packages = hpkgs: [
-              # reuse the nixpkgs for this package
-              hpkgs.distribution-nixpkgs
-              # call our generated Nix expression manually
-              (hpkgs.callPackage ./nix/pkg.nix { })
-            ];
-
-            # development tools we use
-            nativeBuildInputs = [
-              pkgs.cabal-install
-              pkgs.cabal2nix
-              pkgs.hpack
-              pkgs.haskell.packages.ghc912.haskell-language-server
-              pkgs.hyperfine
-              pkgs.ormolu
-            ];
-
-            # Extra arguments are added to mkDerivation's arguments as-is.
-            # Since it adds all passed arguments to the shell environment,
-            # we can use this to set the environment variable the `Paths_`
-            # module of distribution-nixpkgs uses to search for bundled
-            # files.
-            # See also: https://cabal.readthedocs.io/en/latest/cabal-package.html#accessing-data-files-from-package-code
-            distribution_nixpkgs_datadir = toString ./distribution-nixpkgs;
-          };
-
+          default = shellFor hp;
+          profiling = shellFor hpWithProfiling;
         }
       );
 
