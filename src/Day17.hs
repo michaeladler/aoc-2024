27870d80e2a1787cd05891c33253c000d05145ce
Michael Adler <therisen06@gmail.com>
2025-12-30 12:02:16 +0100

    refactor(Day17): use microlenses

--- src/Day17.hs
+++ src/Day17.hs
@@ -7,17 +7,20 @@ import Data.Sequence ((|>))
 import qualified Data.Text as T
 import Data.Vector.Unboxed (Vector)
 import qualified Data.Vector.Unboxed as VU
+import Lens.Micro
+import Lens.Micro.TH
 import Protolude
 
 data Computer = Computer
-  { regA :: !Int,
-    regB :: !Int,
-    regC :: !Int,
-    code :: Vector Int,
-    ip :: !Int,
-    out :: Seq Int
+  { _regA :: !Int,
+    _regB :: !Int,
+    _regC :: !Int,
+    _code :: Vector Int,
+    _ip :: !Int,
+    _out :: Seq Int
   }
-  deriving (Eq, Show)
+
+makeLenses ''Computer
 
 solve :: C.ByteString -> Either Text (Text, Maybe Int)
 solve input = solve' <$> first toS (parseOnly inputParser input)
@@ -31,16 +34,16 @@ inputParser = do
   code' <- decimal `sepBy` char ','
   return
     Computer
-      { regA = a,
-        regB = b,
-        regC = c,
-        code = VU.fromList code',
-        ip = 0,
-        out = mempty
+      { _regA = a,
+        _regB = b,
+        _regC = c,
+        _code = VU.fromList code',
+        _ip = 0,
+        _out = mempty
       }
 
 solve' :: Computer -> (Text, Maybe Int)
-solve' computer = (T.intercalate "," (map show (toList $ out (runComputer computer))), findA computer)
+solve' computer = (T.intercalate "," (map show (toList $ runComputer computer ^. out)), findA computer)
 
 -- run the computer until it halts
 runComputer :: Computer -> Computer
@@ -49,116 +52,64 @@ runComputer computer = maybe computer runComputer (tick computer)
 findA :: Computer -> Maybe Int
 findA computer = fst <$> IntSet.minView (go 0 0 mempty)
   where
-    code' = code computer
-    n = VU.length (code computer)
+    code' = computer ^. code
+    n = VU.length code'
     go :: Int -> Int -> IntSet -> IntSet
     go a i results
       | output == code' = IntSet.insert a results
       | output == VU.slice (n - i) i code' || i == 0 = IntSet.unions [go (8 * a + j) (i + 1) results | j <- [0 .. 7]]
       | otherwise = results
       where
-        computer' =
-          Computer
-            { regA = a,
-              regB = regB computer,
-              regC = regC computer,
-              code = code computer,
-              ip = ip computer,
-              out = out computer
-            }
-        output = VU.fromList (toList (out (runComputer computer')))
+        computer' = computer & regA .~ a
+        output = VU.fromList (toList (runComputer computer' ^. out))
 
 tick :: Computer -> Maybe Computer
-tick Computer {..} =
-  let opcode = code VU.!? ip
-      operand = code VU.! (ip + 1)
+tick computer =
+  let opcode = (computer ^. code) VU.!? (computer ^. ip)
+      operand = (computer ^. code) VU.! (computer ^. ip + 1)
       evalCombo oper
         | oper >= 0 && oper <= 3 = Just oper
-        | oper == 4 = Just regA
-        | oper == 5 = Just regB
-        | oper == 6 = Just regC
+        | oper == 4 = Just (computer ^. regA)
+        | oper == 5 = Just (computer ^. regB)
+        | oper == 6 = Just (computer ^. regC)
         | otherwise = Nothing
       toDouble :: Int -> Double
       toDouble = fromIntegral
    in case (opcode, evalCombo operand) of
         (Just 0, Just combo) ->
-          Just
-            Computer
-              { regA = floor $ toDouble regA / toDouble ((2 :: Int) ^ combo),
-                regB = regB,
-                regC = regC,
-                code = code,
-                ip = ip + 2,
-                out = out
-              }
+          Just $
+            computer
+              & (regA %~ (\a' -> floor (toDouble a' / toDouble (2 ^ combo))))
+              & (ip %~ (+ 2))
         (Just 1, _) ->
-          Just
-            Computer
-              { regA = regA,
-                regB = regB `xor` operand,
-                regC = regC,
-                code = code,
-                ip = ip + 2,
-                out = out
-              }
+          Just $
+            computer
+              & regB %~ (`xor` operand)
+              & ip %~ (+ 2)
         (Just 2, Just combo) ->
-          Just
-            Computer
-              { regA = regA,
-                regB = combo `mod` 8,
-                regC = regC,
-                code = code,
-                ip = ip + 2,
-                out = out
-              }
-        (Just 3, _) ->
-          Just
-            Computer
-              { regA = regA,
-                regB = regB,
-                regC = regC,
-                code = code,
-                ip = if regA /= 0 then operand else ip + 2,
-                out = out
-              }
+          Just $
+            computer
+              & regB .~ (combo `mod` 8)
+              & ip %~ (+ 2)
+        (Just 3, _) -> Just (computer & ip %~ (\ip' -> if (computer ^. regA) /= 0 then operand else ip' + 2))
         (Just 4, _) ->
-          Just
-            Computer
-              { regA = regA,
-                regB = regB `xor` regC,
-                regC = regC,
-                code = code,
-                ip = ip + 2,
-                out = out
-              }
+          Just $
+            computer
+              & regB %~ (`xor` (computer ^. regC))
+              & ip %~ (+ 2)
         (Just 5, Just combo) ->
-          Just
-            Computer
-              { regA = regA,
-                regB = regB,
-                regC = regC,
-                code = code,
-                ip = ip + 2,
-                out = out |> (combo `mod` 8)
-              }
+          Just $
+            computer
+              & out %~ (|> (combo `mod` 8))
+              & ip %~ (+ 2)
         (Just 6, Just combo) ->
-          Just
-            Computer
-              { regA = regA,
-                regB = floor $ toDouble regA / toDouble ((2 :: Int) ^ combo),
-                regC = regC,
-                code = code,
-                ip = ip + 2,
-                out = out
-              }
+          Just $
+            computer
+              & regB .~ floor (toDouble (computer ^. regA) / toDouble (2 ^ combo))
+              & ip %~ (+ 2)
         (Just 7, Just combo) ->
-          Just
-            Computer
-              { regA = regA,
-                regB = regB,
-                regC = floor $ toDouble regA / toDouble ((2 :: Int) ^ combo),
-                code = code,
-                ip = ip + 2,
-                out = out
-              }
+          Just $
+            computer
+              & regC .~ floor (toDouble (computer ^. regA) / toDouble (2 ^ combo))
+              & ip %~ (+ 2)
         _ -> Nothing -- If the computer tries to read an opcode past the end of the program, it instead halts.
